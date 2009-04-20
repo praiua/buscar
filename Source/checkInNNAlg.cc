d1c55ad26edc8bd67218bf34c9549a1f88303555
Jose Oncina <oncina@dlsi.ua.es>
2009-04-20 08:13:38 +0200

    General Makefile changed in order to make an clean all

--- Source/checkInNNAlg.cc
+++
@@ -1,5 +0,0 @@
-#include <map>
-#include "checkInNNAlg.h"
-
-map<string, NNAlg* (*)( vector<string>, Oracle* )> CheckInNNAlg::table;
-map<string, string> CheckInNNAlg::tableInfo;
--- Source/checkInOracle.cc
+++
@@ -1,5 +0,0 @@
-#include <map>
-#include "checkInOracle.h"
-
-map<string, Oracle* (*)( vector<string> )> CheckInOracle::table;
-map<string, string> CheckInOracle::tableInfo;
